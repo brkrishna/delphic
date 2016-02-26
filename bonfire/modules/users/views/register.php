b1f58876d6d6979e51f60b03a94bd28ff8ffc39f
Ramakrishna <brkrishna@gmail.com>
2016-02-26 21:45:54 +0530

    Changed page title

--- bonfire/modules/users/views/register.php
+++ bonfire/modules/users/views/register.php
@@ -24,13 +24,14 @@ if (validation_errors()) :
 <?php echo form_open(site_url(REGISTER_URL), array('class' => "form-horizontal", 'autocomplete' => 'off')); ?>
 
 <div class="row">
-    <hr/><h3>Sign up</h3>
+    <hr/><h3>Registration Step 1</h3>
     <div class="col-xs-12 col-sm-6 col-md-6">
         <div class="form-group<?php echo form_error('email') ? $errorClass : ''; ?>">
             <label class="control-label required" for="email"><?php echo lang('bf_email') . lang('bf_form_label_required') ; ?></label>
             <input type="text" id="email" name="email" class="form-control" value="<?php echo set_value('email', isset($user) ? $user->email : ''); ?>" />
             <span class="help-inline"><?php echo form_error('email'); ?></span>
         </div>
+        <!--
         <div class="form-group<?php echo form_error('display_name') ? $errorClass : ''; ?>">
             <label class="control-label" for="display_name"><?php echo lang('bf_display_name'); ?></label>
             <div class="controls">
@@ -38,9 +39,10 @@ if (validation_errors()) :
                 <span class="help-inline"><?php echo form_error('display_name'); ?></span>
             </div>
         </div>
+        -->
         <?php if (settings_item('auth.login_type') !== 'email' || settings_item('auth.use_usernames')) : ?>
         <div class="form-group<?php echo form_error('username') ? $errorClass : ''; ?>">
-            <label class="control-label required" for="username"><?php echo lang('bf_username'); ?></label>
+            <label class="control-label required" for="username"><?php echo lang('bf_username') . lang('bf_form_label_required'); ?></label>
             <div class="controls">
                 <input class="form-control" type="text" id="username" name="username" value="<?php echo set_value('username', isset($user) ? $user->username : ''); ?>" />
                 <span class="help-inline"><?php echo form_error('username'); ?></span>
@@ -61,6 +63,7 @@ if (validation_errors()) :
                 <span class="help-inline"><?php echo form_error('pass_confirm'); ?></span>
             </div>
         </div>
+        <br/><br/>
         <div class="form-group">
             <?php
             // Allow modules to render custom fields. No payload is passed
