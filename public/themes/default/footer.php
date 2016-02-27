8fdb830425be596bf5ef6ba8292d2c329b41582d
Ramakrishna <brkrishna@gmail.com>
2016-02-27 23:05:01 +0530

    centered footer

--- public/themes/default/footer.php
+++ public/themes/default/footer.php
@@ -2,7 +2,7 @@
     <hr />
     <footer class="footer">
         <div class="row">
-            <p>In case of any queries, please write to : <a href="mailto:register@youthdelhpicgames.com">register@youthdelphicgames.com</a>
+            <p class="text-center">In case of any queries, please write to : <a href="mailto:register@youthdelhpicgames.com">register@youthdelphicgames.com</a>
             or call on <span class="glyphicon glyphicon-earphone"></span>&nbsp;<small>+91-9866 722 247</small></p>
         </div>
         <!--<div class="container">
