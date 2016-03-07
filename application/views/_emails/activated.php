96f7f7d97329b9a4b7e1a947d526603fa920f4bc
Ramakrishna <brkrishna@gmail.com>
2016-03-07 23:38:24 +0530

    corrected email text

--- application/views/_emails/activated.php
+++ application/views/_emails/activated.php
@@ -1,3 +1,18 @@
-<p>Congratulations. Your account on the <?php e($title); ?> site is now registered and active!</p>
+<p>
+Hi,
+<br/>
+Thank you for <b>signing</b> up for the V Youth Delphic Games- India edition. 
+<br/>
+Kindly note, this is an auto-generated email and is only a confirmation of your signing up.
+However, this does not guarantee an approval of your application for the V Youth Delphic Games
+The status of your application form would be mailed to you within 7 days of your registration.
+You may use your username and password to log in to the website to see your profile again.
+You have been added to our mailing list, and will now be among the first to hear about all the competitions, exhibitions and participation in association with the V Youth Delphic Games—right to your inbox.
+<br/><br/>
+In case of any queries, feel free to get in touch with us at info@youthdelphicgames.com
+<br/><br/>
+Cheers!
+<br/><br/>
+</p>
 
 <p><a href="<?php echo $link ?>"><?php echo $link ?></a></p>
